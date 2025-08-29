a95d29da4936055c429ff60bf260348d696e975b
Marko101ish <obradovicmarko1999@gmail.com>
2025-08-30 01:31:18 +0200

    Added grass V0
    Added grass on the floor block, no support for expanding floor downwards (should be handled when implementing Camera)

--- src/main/java/rs/etf/dz1/sprites/FloorBlock.java
+++ src/main/java/rs/etf/dz1/sprites/FloorBlock.java
@@ -5,8 +5,12 @@
  */
 package rs.etf.dz1.sprites;
 
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Polygon;
+
+import java.util.Random;
 
 /**
  *
@@ -14,12 +18,34 @@ import javafx.scene.shape.Rectangle;
  */
 public class FloorBlock extends Sprite {
 
-    private Rectangle block;
+    private Rectangle background;
+    private Polygon foreground;
 
     public FloorBlock() {
-        block = new Rectangle(-0.5, -0.5, 1, 1);
-        block.setFill(Color.SIENNA);
-        getChildren().add(block);
+        foreground = new Polygon();
+
+        // Grass polygon
+        //        1                       7  ======== y =  -0.5  - top of the block
+        //              3           5        ======== y = -0.25 - quarter from top
+        //        2           4           6  ======== y =   0.0 - middle of the block
+        //
+        // x = -0.5  -0.25   0.0   0.25  0.5
+        foreground.getPoints().addAll(new Double[]{
+                 -0.5,  -0.5,   // 1
+                 -0.5,   0.0,   // 2
+                -0.25, -0.25,   // 3
+                  0.0,   0.0,   // 4
+                 0.25, -0.25,   // 5
+                  0.5,   0.0,   // 6
+                  0.5,  -0.5,   // 7
+        });
+
+        background = new Rectangle(-0.5, -0.5, 1, 1);
+        Random rand = new Random();
+        background.setFill(Color.SIENNA);
+        foreground.setFill(Color.LAWNGREEN);
+        getChildren().add(background);
+        getChildren().add(foreground);
     }
 
 }
